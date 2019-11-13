db6ed185ce7b422718c486448db655b2181a0ac6
Heather Kim <haesoo1108@gmail.com>
2019-11-13 10:34:53 +0900

    Initial Commit

---
+++ MeetingCalendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeetingCalendar
+//
+//  Created by Haesoo Kim on 13/11/2019.
+//  Copyright © 2019 Haesoo Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
