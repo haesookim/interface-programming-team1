b1ad9bf40badc537c77685a13112308e944a1b24
SooMin Lee <soomin8142@gmail.com>
2019-12-02 23:37:15 +0900

    - few changes in data structure - addition of tableview functions in viewcontroller - additional storyboard in case of co-working

--- MeetingCalendar/PlanList.swift
+++ MeetingCalendar/PlanList.swift
@@ -8,11 +8,31 @@
 
 import Foundation
 
+
+struct cellData{ //this shall be merged into planlist in order to be coherent
+    
+    var dates : String //using string for test - need to adjust suitable format - date?
+    var plans : [String]//[Plan] //using string for test
+    
+    
+}
+
 class PlanList {
     var planList: [Plan] = [] // Init as empty array
     
-    init(){
+    var planDate : String = "" // will be used as section data, which are dates (may use string?)
+    
+    
+    init(date: String){
         // TODO: Load items in from database and append to planList
+        
+        // need default initializing
+        // if section for plandate already exist, addNewPlan shall be ran, not init.
+        // if section for plandate does not exist, init and addNewPlan there.
+        // in conclusion, only consider case where new planlist for a certain date is needed
+        planDate = "19/12/24"
+//        planList = [Plan(date: planDate, time: "19:00", category: Category.Friend, peopleCount: 1, meetingWith: "", notes: "")]
+        
     }
     
     func addNewPlan(date: String, time: String, category: Category, peopleCount: Int, meetingWith: String?, notes: String?) -> Plan {
